7f4ad6844c90558288cfa8f445470a51317d2441
AnantGamer280 <anantgamer695@gmail.com>
2025-02-28 03:56:02 -0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+# Pehle sab files ko add karo
+git add .
+
+# Ek commit banao
+git commit -m "Initial commit: Added Docker setup for Render.com"
+
+# Branch ko push karo
+git branch -M main
+git push -u origin main
